863d1dfb62038988576becab6f2422c3e8ad4e1d
vijay1923 <vijaymagadum1923@gmai.com>
2026-01-28 10:37:20 +0530

    variables in rust

---
+++ variables/shadowing/target/debug/deps/shadowing-16442d2c9291c444.d
@@ -0,0 +1,5 @@
+/home/assasinbaby/rust/variables/shadowing/target/debug/deps/shadowing-16442d2c9291c444.d: src/main.rs
+
+/home/assasinbaby/rust/variables/shadowing/target/debug/deps/shadowing-16442d2c9291c444: src/main.rs
+
+src/main.rs:
---
+++ variables/var_1/target/debug/deps/var_1-5cb174ae0c1e028c.d
@@ -0,0 +1,5 @@
+/home/assasinbaby/rust/variables/var_1/target/debug/deps/var_1-5cb174ae0c1e028c.d: src/main.rs
+
+/home/assasinbaby/rust/variables/var_1/target/debug/deps/var_1-5cb174ae0c1e028c: src/main.rs
+
+src/main.rs:
