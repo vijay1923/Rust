388e75ce5ef4640c0290063c9e8654295949804f
Vijay Magadum <82163990+vijay1923@users.noreply.github.com>
2026-05-22 15:20:54 +0530

    Delete ownship/ownership_and_functions/string_type/string_type directory

--- ownship/ownership_and_functions/string_type/string_type/target/debug/deps/string_type-fb5d9665c84ebe91.d
+++
@@ -1,5 +0,0 @@
-/home/assasinbaby/rust/ownship/string_type/string_type/target/debug/deps/string_type-fb5d9665c84ebe91.d: src/main.rs
-
-/home/assasinbaby/rust/ownship/string_type/string_type/target/debug/deps/string_type-fb5d9665c84ebe91: src/main.rs
-
-src/main.rs:
--- ownship/ownership_and_functions/string_type/string_type/target/debug/string_type.d
+++
@@ -1,1 +0,0 @@
-/home/assasinbaby/rust/ownship/string_type/string_type/target/debug/string_type: /home/assasinbaby/rust/ownship/string_type/string_type/src/main.rs
